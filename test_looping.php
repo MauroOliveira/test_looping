1c0bdf350644e60f0e8ea893816e20f45a617a1a
Mauro César de Oliveira <mauro_cesarde@yahoo.com.br>
2015-09-15 13:24:06 -0300

    test_looping
    test_looping

--- test_looping.php
+++ test_looping.php
@@ -1,10 +1,14 @@
 <?php
 
+//html post variable
 $looping = $_POST['looping'];
+//clear simple counter
 $i = 0;
 
 while ($looping <= 87) {
+	//account quantity looping set
 	$looping = $looping + 1;
+	//simple counter
 	$i = $i + 1; 
 	echo $i . '<h5>numero errado</h5>';
 }
